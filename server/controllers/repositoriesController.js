b05cb629e6452b773f75077146a7f33774f9dfff
NayeraMohamed <nayeramoh7@gmail.com>
2021-05-07 16:17:07 +0200

    fixed data paramater

--- server/controllers/repositoriesController.js
+++ server/controllers/repositoriesController.js
@@ -3,11 +3,12 @@ const octokit = require('@octokit/request')
 exports.getTrendingRepos = async (req, res) => {
     try {
         const today = new Date();
-        const priorDate = new Date().setDate(today.getDate() - 30);
-        const date = new Date(priorDate).toISOString(); //to convert it from milli seconds to YYYY-MM-DD format
+        const priorDate = new Date().setDate(today.getDate() - 30); //date in milliseconds
+        //to convert date from milli seconds to YYYY-MM-DD format and remove time zone
+        const date = new Date(priorDate).toISOString().split('T')[0]; 
         console.log(date);
         const repos = await octokit.request('GET /search/repositories', {
-          q: 'created:>date', //check syntax
+          q: `created:>${date} `, //check syntax
           sort: 'stars',
           order: 'desc',
           per_page: '100',
