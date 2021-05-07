de0852eb57f44f8d2ca236a7ca30f554cb627fd2
NayeraMohamed <nayeramoh7@gmail.com>
2021-05-07 17:45:51 +0200

    added getTrendingLanguagesInRepos route
    Lists languages used by trending repos along with:
    -Number of repos using this language
    -The list of repos using the language

--- server/controllers/repositoriesController.js
+++ server/controllers/repositoriesController.js
@@ -1,21 +1,42 @@
 const octokit = require('@octokit/request')
 
-exports.getTrendingRepos = async (req, res) => {
+getTrendingRepos = async () => {
     try {
         const today = new Date();
         const priorDate = new Date().setDate(today.getDate() - 30); //date in milliseconds
         //to convert date from milli seconds to YYYY-MM-DD format and remove time zone
         const date = new Date(priorDate).toISOString().split('T')[0]; 
-        console.log(date);
         const repos = await octokit.request('GET /search/repositories', {
-          q: `created:>${date} `, //check syntax
+          q: `created:>${date} `,
           sort: 'stars',
           order: 'desc',
           per_page: '100',
         })
-        return res.send({data: repos.data.items})
+        return repos.data.items;
     
       } catch (err) {
-        return res.send(err.message)
+        return err;
       }
 }
+
+exports.getTrendingLanguagesInRepos = async (req, res) => {
+  
+  try {
+    const trendingRepos = await getTrendingRepos();
+    let languages = {};
+    for (let i = 0; i < trendingRepos.length; i++) {
+      const repo = trendingRepos[i];
+      const language = trendingRepos[i].language;
+      languages[language] = languages[language] || [];
+      languages[language].push(repo);
+    }
+    let TrendingLanguages = [];
+    for (const language in languages) {
+      TrendingLanguages.push({language: language, count: languages[language].length, repos: languages[language]})
+      console.log(languages[language].length)  
+      }
+    return res.send( {data: TrendingLanguages} );
+  } catch (err) {
+    return res.send(err.message);
+  }
+};
--- server/routes/repositories.js
+++ server/routes/repositories.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const ReposController = require('../controllers/repositoriesController');
 
-router.get("/getTrendingRepositories", ReposController.getTrendingRepos);
+//router.get("/getTrendingRepositories", ReposController.getTrendingRepos);
+
+router.get("/getTrendingLanguagesInRepos", ReposController.getTrendingLanguagesInRepos);
 
 module.exports = router;
